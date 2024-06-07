c61286e1d7e47eec2d6e304216fdbdde5633336a
Matheus Gurgel <88843455+mathgurgel@users.noreply.github.com>
2024-06-07 12:38:20 +0000

    feat: add tags to card

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ function App() {
     <>
       <div className="card">
         <h2>Naruto Uzumaki</h2>
+        <div className="tags">
+          <div>Clã: Uzumaki</div>
+          <div>Time: 7</div>
+          <div>Aldeia: Folha</div>
+        </div>
         <img src="https://static.wikia.nocookie.net/naruto/images/3/33/Naruto_Uzumaki_%28Parte_I_-_HD%29.png/revision/latest/scale-to-width/360?cb=20160316113315&path-prefix=pt-br" />
       </div>
     </>
