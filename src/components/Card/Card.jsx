c7c9a89973f4526688e8de1f402a2f5c7e95669e
Matheus Gurgel <88843455+mathgurgel@users.noreply.github.com>
2024-06-13 14:43:20 +0000

    refactor: change jutsu tag to rank

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,10 +5,9 @@ export default function Card(props) {
         <div className="card">
             <h2>{item.name}</h2>
             <div className="tags">
-                <div>Village: {item.personal.affiliation[0]}</div>
-                <div>Team: {item.personal.team ? item.personal.team[0] : 'None'}</div>
-                <div>Jutsu: {item.jutsu ? item.jutsu[0] : 'None'}</div>
-                
+                <div>Village: {item.personal?.affiliation?.[0] ?? 'None'}</div>
+                <div>Team: {item.personal?.team?.[0] ?? 'None'}</div>
+                <div>Rank: {item.rank?.ninjaRank?.["Part I"] ?? 'None'}</div>
             </div>
             <img src={item.images[0]} />
         </div>
